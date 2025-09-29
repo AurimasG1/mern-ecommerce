01ed5c41aacc16accaa87f1714f91eeb3839e528
AurimasG1 <112963534+AurimasG1@users.noreply.github.com>
2025-09-29 22:38:55 +0300

    v4

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -2,38 +2,29 @@ import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
 
 export const protectRoute = async (req, res, next) => {
+	const accessToken = req.cookies.accessToken;
+	if (!accessToken) {
+		return res.status(401).json({ message: 'You need to login' });
+	}
 	try {
-		const accessToken = req.cookies.accessToken;
-
-		if (!accessToken) {
-			return res.status(401).json({ message: 'You need to login' });
-		}
-
-		let decoded;
-
-		try {
-			decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-		} catch (error) {
-			if (error.name === 'TokenExpiredError') {
-				return res
-					.status(401)
-					.json({ message: 'Unauthorized - Access token expired' });
-			}
-			return res.status(401).json({ message: 'Invalid access token' }); // catch all other JWT errors
-		}
+		const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
 		const user = await User.findById(decoded.userId).select('-password');
-
 		if (!user) {
 			return res.status(401).json({ message: 'User not found' });
 		}
-
 		req.user = user;
 
 		next(); // continue to route
 	} catch (error) {
-		console.log('Error in protectRoute middleware', error.message);
-		res.status(500).json({ message: 'Unauthorized - Invalid access token' });
+		if (error.name === 'TokenExpiredError') {
+			return res
+				.status(401)
+				.json({ message: 'Unauthorized - Access token expired' });
+		}
 	}
+
+	console.log('Error in protectRoute middleware', error.message);
+	return res.status(401).json({ message: 'Invalid access token' }); // catch all other JWT errors
 };
 
 export const adminRoute = (req, res, next) => {
